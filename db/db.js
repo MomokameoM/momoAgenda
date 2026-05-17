dd51ca4eaadc0e752ac9dd2246d7f58f6ff3fedc
Momo <flores.leyva.murphy.alexander@gmail.com>
2026-05-17 17:45:04 -0600

    herokudb

--- db/db.js
+++ db/db.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql2');
 
 // conexión
-const connection = mysql.createConnection({
+const connection = mysql.createConnection({/*
   host: 'localhost',
   user: 'root',
   password: 'momo',
-  database: 'momoAgenda'
+  database: 'momoAgenda'*/
+  host: 'zj2x67aktl2o6q2n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: 'oiu1k0ag19b1z2nb',
+  password: 'x4tjv25abzi5b4bj',
+  database: 'u601ae9j9854yz56'
 });
 
 
